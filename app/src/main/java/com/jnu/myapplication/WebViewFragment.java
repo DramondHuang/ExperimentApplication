92b550f5c02f0067918f2fc64be346da3bf52f8f
Your Name <you@example.com>
2022-12-02 21:32:16 +0800

    Finish BaiduMap

--- app/src/main/java/com/jnu/myapplication/WebViewFragment.java
+++ app/src/main/java/com/jnu/myapplication/WebViewFragment.java
@@ -13,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baidu.mapapi.SDKInitializer;
+
 public class WebViewFragment extends Fragment {
     private WebView webview;
     @Override
@@ -35,13 +37,22 @@ public class WebViewFragment extends Fragment {
         settings.setSupportZoom(true);//设定支持缩放
         settings.setBlockNetworkLoads(false);
 
+
         //打开的网址
         webview.loadUrl("https://news.sina.cn");
-        webview.setWebChromeClient(new WebChromeClient(){
+        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
+        webview.setWebViewClient(new WebViewClient(){
+            @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //使用WebView加载显示url
                 view.loadUrl(url);
+                //返回true
                 return true;
             }
         });
+    }
+    public void onResume() {
+        webview.loadUrl("https://news.sina.cn");
+        super.onResume();
     }
 }
